aaf751887a517f575b2d4269c7221dc0747ef4e9
gihr0cha <104589261+gihr0cha@users.noreply.github.com>
2024-04-28 16:17:29 -0300

    pagina de dados

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'sessao_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -19,50 +20,7 @@ class HomePage extends StatelessWidget {
             showDialog(
               context: context,
               builder: (BuildContext context) {
-                return Dialog(
-                  child: Container(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        TextField(
-                          maxLines: null,
-                          decoration: InputDecoration(
-                            hintText: 'Enter your text here',
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        ElevatedButton(
-                          child: Text('Next'),
-                          onPressed: () {
-                            Navigator.of(context).pop(); // Close current dialog
-                            showDialog( // Open new dialog
-                              context: context,
-                              builder: (BuildContext context) {
-                                return Dialog(
-                                  child: Container(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: TextField(
-                                      maxLines: null,
-                                      decoration: InputDecoration(
-                                        hintText: 'Enter more data here',
-                                        filled: true,
-                                        fillColor: Colors.white,
-                                        border: OutlineInputBorder(),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+                return MyDialog();
               },
             );
           },
